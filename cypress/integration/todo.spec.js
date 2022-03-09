f476bbe5c39736c9093b272709ec1cbdabcf15c2
Dongus <bjarkim19@ru.is>
2022-03-09 12:10:15 +0000

    Added test for empty todo list

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,3 +1,13 @@
+/// <reference types="cypress" />
+
+
 describe("📝 TODO app", () => {
-  // TODO Add your Cypress tests here
-});
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  }) 
+
+  it('Should have an empty todo list before get api/list', () => {
+    cy.get('[data-testid=todo-item]').should('not.exist')
+  })
+})
+
