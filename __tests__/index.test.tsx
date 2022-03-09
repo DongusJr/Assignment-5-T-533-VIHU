124b37e92a0db36fdd9b0f0bb4d96dbf06c210c7
Dongus <bjarkim19@ru.is>
2022-03-09 14:37:28 +0000

    test: add a new todo item and submit

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,13 +4,19 @@ import { rest } from "msw"
 import { setupServer } from "msw/node";
 import "@testing-library/jest-dom";
 import Home from "../pages/index";
+import { act } from "react-dom/test-utils";
 
 // Good starting point: https://testing-library.com/docs/react-testing-library/example-intro
 
 // TODO setup your mock api here
+const newTodo = "Add a new Todo to the Todo list ✔"
+
 const server = setupServer(
   rest.get("/api/list", (req, res, ctx) => {
-    return res(ctx.json([{ title: "Test the program 🧪"}]));
+    return res(ctx.json([{id: "1", title: "Test the program 🧪"}]));
+  }),
+  rest.post("api/add", (req, res, ctx) => {
+    return res(ctx.json({id: "2", title: newTodo}));
   })
 );
 
@@ -39,5 +45,26 @@ describe("📝 TODO app", () => {
     await waitFor(() => screen.getByTestId("todo-item"))
 
     expect(screen.getAllByTestId("todo-item")).toHaveLength(1);
+  });
+
+  it('should add a new todo item on todo-input submit', async () => {
+    render(<Home />);
+    // Wait for api/list
+    await waitFor(() => screen.getByTestId("todo-item"))
+
+    // Input should initially be empty
+    expect(screen.getByTestId("todo-input")).toHaveValue("");
+
+    // Submit a new todo that is not ""
+    fireEvent.change(screen.getByTestId("todo-input"), {target: {value: newTodo}})    
+    fireEvent.submit(screen.getByTestId("todo-input"))
+
+    // On submit should clear the field
+    expect(screen.getByTestId("todo-input")).toHaveValue("")
+
+    // Check if the new item has been added
+    await waitFor(() => expect(screen.getAllByTestId("todo-item")).toHaveLength(2))
+    expect(screen.getAllByTestId("todo-item").slice(-1)[0]).toHaveTextContent(newTodo)
+  
   });
 })
